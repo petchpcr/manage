5d03da7cae56d91db172114869276113282c4ceb
petchpcr <petchpcr@gmail.com>
2019-06-29 23:07:45 +0700

    login > main

--- page/menu.php
+++ page/menu.php
@@ -13,7 +13,7 @@
 
   <!-- Nav Item - Dashboard -->
   <li class="nav-item active">
-    <a class="nav-link" href="main.php">
+    <a class="nav-link" href="page/main.php">
       <i class="fas fa-fw fa-tachometer-alt"></i>
       <span>Dashboard</span></a>
   </li>
--- page/navbar.php
+++ page/navbar.php
@@ -8,7 +8,7 @@
     <!-- Topbar Search -->
     <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
     <div class="input-group">
-        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2" style="margin-top:0.1rem">
         <div class="input-group-append">
         <button class="btn btn-warning" type="button">
             <i class="fas fa-search fa-sm"></i>
