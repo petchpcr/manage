215f13f4d8d824b6e71b3ca929610a490f671f1b
petchpcr <petchpcr@gmail.com>
2019-07-14 16:44:02 +0700

    list

--- page/script_css.php
+++ page/script_css.php
@@ -4,4 +4,5 @@
 
 <!-- Custom styles for this template-->
 <link href="../css/sb-admin-2.css" rel="stylesheet">
+<link href="../css/chomthong.css" rel="stylesheet">
 <link href="../vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
